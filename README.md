9caad63feeb65cbe2dda12ad86c6f16e7c490077
Brian Ingles <brianingles@deephaven.io>
2024-02-26 14:55:06 -0600

    Cleanup

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Ts Inspector
 
 Inpsect TypeScript types using compiler api
+
+## Usage
+
+Build the script
+
+```sh
+npm run build
+```
+
+Run the script with target file and optional identifiers args
+
+```sh
+# Extract `SpectrumPickerProps` type from `@react-types/select` package
+npm start \
+ node_modules/@react-types/select/src/index.d.ts \
+ SpectrumPickerProps
+```
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
 import * as ts from 'typescript';
 
+if (process.argv.length < 3) {
+  console.error(
+    `Missing required <targetFile> argument.
+
+Usage: node . <targetFile> [identifiers]
+
+  targetFile: The file to extract top level identifiers from.
+
+  identifiers: Optional list of top level identifiers to extract from the target file.
+`
+  );
+  process.exit(1);
+}
+
+const [targetFile, ...identifiers] = process.argv.slice(2);
+extract(targetFile, identifiers);
+
 /**
- * Prints out particular nodes from a source file
+ * Prints out property info for top level types from a source file
  *
  * @param file a path to a file
  * @param identifiers top level identifiers available
@@ -17,13 +34,6 @@ function extract(file: string, identifiers: string[]): void {
     return;
   }
 
-  // To print the AST, we'll use TypeScript's printer
-  const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
-
-  // To give constructive error messages, keep track of found and un-found identifiers
-  const unfoundNodes: [string, ts.Node][] = [],
-    foundNodes: [string, ts.Node][] = [];
-
   // Loop through the root AST nodes of the file
   ts.forEachChild(sourceFile, node => {
     if (ts.isInterfaceDeclaration(node) || ts.isTypeAliasDeclaration(node)) {
@@ -35,21 +45,25 @@ function extract(file: string, identifiers: string[]): void {
       }
 
       console.log(`\n${name}`);
+
       const type = typeChecker.getTypeAtLocation(node);
+
+      // TODO: Group results by categories like in the Spectrum docs:
+      // https://github.com/adobe/react-spectrum/blob/fa3862eab668b43459dd99f7fd4bf74e6eda492b/packages/dev/docs/src/PropTable.js#L18
       typeChecker.getPropertiesOfType(type).forEach(prop => {
         const propName = prop.escapedName;
+
         const propComment = prop
           .getDocumentationComment(typeChecker)
           .map(c => c.text)
           .join('\n');
+
         const propType = typeChecker.typeToString(
           typeChecker.getTypeOfSymbol(prop)
         );
+
         console.log(`\n/** ${propComment} */\n${propName}: ${propType}`);
       });
     }
   });
 }
-
-// Run the extract function with the script's arguments
-extract(process.argv[2], process.argv.slice(3));
